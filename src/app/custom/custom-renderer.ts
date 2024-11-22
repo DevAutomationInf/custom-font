0ff9d3a8933535a2b0576ec24b303d04a80aec8c
gnitulescu <gabriela.nitulescu@infor.com>
2024-11-22 17:52:52 +0100

    bpmn group label resize

---
+++ src/app/custom/custom-renderer.ts
@@ -0,0 +1,62 @@
+import BaseRenderer, { BpmnRendererConfig } from 'bpmn-js/lib/draw/BpmnRenderer.js';
+import { getLabelColor } from 'bpmn-js/lib/draw/BpmnRenderUtil';
+import PathMap from 'bpmn-js/lib/draw/PathMap';
+import TextRenderer from 'bpmn-js/lib/draw/TextRenderer';
+import { getLabel } from 'bpmn-js/lib/features/label-editing/LabelUtil';
+import { Shape } from 'bpmn-js/lib/model/Types';
+import EventBus from 'diagram-js/lib/core/EventBus';
+import Styles from 'diagram-js/lib/draw/Styles';
+import Canvas from 'diagram-js/lib/core/Canvas';
+import TextUtil from 'diagram-js/lib/util/Text';
+import { append as svgAppend} from 'tiny-svg';
+
+export class CustomRenderer extends BaseRenderer {
+  static override $inject = ['config.bpmnRenderer', 'eventBus', 'styles', 'pathMap', 'canvas', 'textRenderer'];
+  textRenderer: TextRenderer;
+  bpmnRenderer: BpmnRendererConfig;
+  private readonly svgBox = {
+    width: 39,
+    height: 39,
+  };
+
+  constructor(
+    bpmnRenderer: BpmnRendererConfig,
+    eventBus: EventBus,
+    styles: Styles,
+    pathMap: PathMap,
+    canvas: Canvas,
+    textRenderer: TextRenderer,
+  ) {
+    super(bpmnRenderer, eventBus, styles, pathMap, canvas, textRenderer, 1500);
+    this.bpmnRenderer = bpmnRenderer;
+    this.textRenderer = textRenderer;
+  }
+
+  override canRender(element: Shape): boolean {
+    return (element.type === 'label' && element.businessObject.$type === 'bpmn:Group');
+  }
+
+  override drawShape(parentGfx: SVGElement, element: Shape): SVGElement {
+      const textUtil = new TextUtil({
+        style: {
+          fontFamily: 'Arial, sans-serif',
+          fontSize: 12,
+          fontWeight: 'normal',
+          lineHeight: 1.2,
+        },
+      });
+      const label = getLabel(element);
+      const box = this.textRenderer.getExternalLabelBounds({ width: 90, height: 30, x: 0, y: 9.8 }, label);
+      let text = textUtil.createText(label, {
+        align: 'center-top',
+        box,
+      });
+      if (element.di?.label?.bounds) {
+        const { width, height } = element.di.label.bounds;
+        text = textUtil.createText(getLabel(element), { box: { width, height } });
+      }
+      svgAppend(parentGfx, text);
+
+      return text;
+  }
+}
